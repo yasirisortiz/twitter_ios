dbf95c85cd228a4f5af81cdaa589530162290dd9
Yasiris Ortiz <yasirisortiz24@gmail.com>
2020-10-17 23:43:12 -0400

    changes favorite and retweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,9 +18,18 @@ class HomeTableTableViewController: UITableViewController {
         super.viewDidLoad()
         loadTweets(moreTweet: false)
         refresh.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = refresh
+        self.tableView.refreshControl = refresh
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+        
 
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.loadTweets(moreTweet: false)
+    }
+    
     @objc func loadTweets(moreTweet: Bool) {
         if !moreTweet {
             numberOfTweet = 20
@@ -71,8 +80,13 @@ class HomeTableTableViewController: UITableViewController {
             cell.profileImage.image = UIImage(data: imageData)
         }
         
+        
         cell.profileName.text = user["name"] as? String
         cell.tweetContent.text = tweetsArray[indexPath.row]["text"] as? String
+        cell.tweetId = tweetsArray[indexPath.row]["id"] as! Int
+        cell.setFavorite((tweetsArray[indexPath.row]["favorited"] as? Bool)!)
+        cell.retweeted = (tweetsArray[indexPath.row]["retweeted"] as? Bool)!
+        //cell.setRetweet((tweetsArray[indexPath.row]["retweeted"] as? Bool)!)
         
         return cell
     }
@@ -86,59 +100,5 @@ class HomeTableTableViewController: UITableViewController {
         return tweetsArray.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Twitter/tweetCell.swift
+++ Twitter/tweetCell.swift
@@ -9,15 +9,93 @@
 import UIKit
 
 class tweetCell: UITableViewCell {
-
+    
+    var tweetId:Int = -1
+    var favorited:Bool = false
+    var retweeted: Bool = false
+    
+    
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var profileName: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBOutlet weak var favButton: UIButton!
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorite = !favorited
+        
+        if (toBeFavorite){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("Favorite did not success: \(Error)")
+            })
+            
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                print("Unfavorite did not success: \(Error)")
+            })
+            
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        let toBeRetweet = !retweeted
+        
+        if (toBeRetweet){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setRetweet(true)
+            }, failure: { (Error) in
+                print("Favorite did not success: \(Error)")
+            })
+            
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setRetweet(false)
+            }, failure: { (Error) in
+                print("Unfavorite did not success: \(Error)")
+            })
+            
+        }
+        
+        
+    }
+        func setFavorite(_ isFavorited:Bool){
+            favorited = isFavorited
+            if (favorited){
+                favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+                
+            }
+            else {
+                favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+            }
+        }
+    
+    func setRetweet(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+            
+        }
+        else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
+
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
